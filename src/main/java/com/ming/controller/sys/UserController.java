f439d89de3ca4bce39f76dd2848ca7e673ce851d
liming_pro <522724933@qq.com>
2021-07-18 20:07:51 +0800

    # 1

--- src/main/java/com/ming/controller/sys/UserController.java
+++ src/main/java/com/ming/controller/sys/UserController.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.ming.entity.User;
 import com.ming.service.IUserService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
